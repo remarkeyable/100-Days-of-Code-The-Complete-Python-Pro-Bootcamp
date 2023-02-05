26060734a942680098e786633bfddf00dfe54278
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-02-05 23:28:53 +0800

    Create main.py

---
+++ Day 26/main.py
@@ -0,0 +1,5 @@
+df = pandas.read_csv("nato_phonetic_alphabet.csv")
+phonetic = {row.letter:row.code for (index,row) in df.iterrows()}
+word = input("Enter a word: ").upper()
+result = [phonetic[letter] for letter in word]
+print(result)

858272dfbde63e2165ddb614533852498b86512d
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-02-05 23:21:15 +0800

    Update game.py

--- Day 25/game.py
+++ Day 25/game.py
@@ -1,18 +1,25 @@
 from turtle import Turtle, Screen
 
+
 class Game(Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
         self.score = 0
+        
+        
     def generate_states(self,x,y,state_name):
         state = Turtle()
         state.hideturtle()
         state.penup()
         state.goto(x,y)
         state.write(state_name)
+        
+        
     def scores(self):
        self.score += 1
+    
+    
     def track(self):
         sc = self.score
         return sc

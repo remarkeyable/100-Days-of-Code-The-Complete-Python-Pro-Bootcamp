77240483f9e9adfd858feca7c369d3d2941aeee5
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-05-16 20:14:43 +0800

    Update main.py

--- Day 84/main.py
+++ Day 84/main.py
@@ -8,47 +8,24 @@ from tkinter.filedialog import askopenfile
 
 
 window = customtkinter.CTk()
-window.minsize(width=800,height=800)
+window.maxsize(width=600,height=600)
 window.title("Katara")
-import time
 def image_loc():
     image = filedialog.askopenfile(mode='r', filetypes=[('Jpg Files', '*.jpg'),('PNG Files','*.png'),('TIFF Files','*.tiff'),('HEIC Files','*.heic')])
     if image:
         global  my_label
         image_path = os.path.abspath(image.name)
         print(image_path)
-
-
-        test = Image.open(fr"{image_path}")
-
-        if test.width > 540 and test.height > 540:
-            resized_image = test.resize((540, 540), Image.Resampling.LANCZOS)
-        elif test.width > 540:
-            resized_image = test.resize((540, test.height), Image.Resampling.LANCZOS)
-        elif test.height > 540:
-            resized_image = test.resize((test.width, 540), Image.Resampling.LANCZOS)
-        else:
-            resized_image = test
-
-        # resize = test.resize((200,200),  Image.Resampling.LANCZOS)
-        aspen = ImageTk.PhotoImage(resized_image)
-        my_label.config(image=aspen)
+        img1 = Image.open(fr"{image_path}")
+        img1.thumbnail((500,500))
+        img2 = ImageTk.PhotoImage(img1)
+        my_label.config(image=img2)
         my_label.pack(pady=20)
-        my_label.image = aspen
-
-
-# def view_pic():
-#     aspen = ImageTk.PhotoImage(file=fr"{image_loc()}")
-#     print("done")
-#     my_label.config(window, image=aspen)
-#     my_label.pack(pady=20)
-#     print(image_loc())
-
+        my_label.image = img2
 
 
 button = customtkinter.CTkButton(window, text="test",  command=image_loc)
 button.pack()
-#
 aspen = ImageTk.PhotoImage(file="images/aa.jpg")
 my_label = Label(window, image=aspen)
 my_label.pack(pady=20)

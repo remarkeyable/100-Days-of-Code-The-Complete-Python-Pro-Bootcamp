b14a953817e6372e8f9aa1d5e4fe27e4f9ca1979
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-05-18 22:17:02 +0800

    Update main.py

--- Day 84/main.py
+++ Day 84/main.py
@@ -10,12 +10,13 @@ import time
 window = customtkinter.CTk()
 customtkinter.set_appearance_mode("dark")
 customtkinter.set_default_color_theme("dark-blue")
-window.minsize(width=600, height=700)
-window.maxsize(width=600, height=700)
+window.minsize(width=450, height=750)
+window.maxsize(width=450, height=750)
 window.title("Katara")
 
 loc = ""
 f_size = 0
+
 
 def image_loc():
     global loc
@@ -34,13 +35,14 @@ def image_loc():
         my_label.image = img2
 
 
-def submit(value):
+def submit():
     global loc
     global mark_entry
     global my_label
 
     file_name = os.path.basename(loc)
     img1 = Image.open(fr"{loc}")
+    print(img1.size)
     img2 = ImageTk.PhotoImage(img1)
     my_label.config(image=img2)
     my_label.pack(pady=20)
@@ -48,8 +50,12 @@ def submit(value):
 
     draw = ImageDraw.Draw(img1)
     text = mark_entry.get()
-    font = ImageFont.truetype("images/Bertha.ttf", int(value))
-    draw.text((10, 10), text=text, font=font)
+    font = ImageFont.truetype("images/Bertha.ttf", 30)
+    w, h = img1.size
+    width = int(w - 500)
+    height = int(h - 300)
+    print(width)
+    draw.text((597, height), text=text, font=font)
     img1.save(f"edited/edited_{file_name}")
 
     edited = Image.open(f"edited/edited_{file_name}")
@@ -67,20 +73,55 @@ def slider_event(value):
     f_size = value
 
 
-
-button = customtkinter.CTkButton(window, text="Open image", command=image_loc)
-button.place(y=450, x=230)
 img = ImageTk.PhotoImage(file="images/1.jpg")
-my_label = Label(window, image=img, width=400, height=400, borderwidth=1, bg="#282424")
+my_label = Label(window, image=img, width=400, height=400, borderwidth=1, bg="#2e2a2a")
 my_label.pack(pady=20)
-mark_entry = customtkinter.CTkEntry(window, placeholder_text="Text watermark")
-mark_entry.place(y=490, x=230)
-font_size = customtkinter.CTkSlider(window, from_=0, to=400, width=140, command=submit)
-font_size.place(y=530, x=228)
 
-fonts = customtkinter.CTkComboBox(window, values=["a","b","c"])
-fonts.place(y=560, x=230)
-submit_button = customtkinter.CTkButton(window, text="Submit", command=submit)
-submit_button.place(y=600, x=230)
+tabview = customtkinter.CTkTabview(master=window, fg_color="#2e2a2a", width=400, height=400)
+tabview.pack(padx=20, pady=20)
+
+tabview.add("Text Watermark")  # add tab at the end
+tabview.add("Logo Watermark")  # add tab at the end
+tabview.set("Text Watermark")  # set currently visible tab
+
+# Text Tab
+button = customtkinter.CTkButton(master=tabview.tab("Text Watermark"), text="Open image", command=image_loc, width=200)
+button.place(y=15, x=95)
+
+mark_entry = customtkinter.CTkEntry(master=tabview.tab("Text Watermark"), placeholder_text="Text watermark", width=200)
+mark_entry.place(y=55, x=95)
+
+set_value = StringVar(value='Top Right')
+position = customtkinter.CTkComboBox(master=tabview.tab("Text Watermark"), values=["Top Right", "Top Left", "Down Right", "Down Left", "Middle", "Middle Top", "Middle Down"],
+                                     width=200, state="readonly", variable=set_value)
+position.place(y=95, x=95)
+
+font_size = customtkinter.CTkComboBox(master=tabview.tab("Text Watermark"), values=["1", "2"], width=60)
+font_size.place(y=135, x=235)
+
+f_set_value = StringVar(value='Fonts')
+fonts = customtkinter.CTkComboBox(master=tabview.tab("Text Watermark"), values=["adsadasdasdasdasdasd", "b", "c"],
+                                  width=130)
+fonts.place(y=135, x=95)
+
+
+def test():
+    print(position.get())
+
+
+submit_button = customtkinter.CTkButton(master=tabview.tab("Text Watermark"), text="Submit", command=submit, width=200)
+submit_button.place(y=175, x=95)
+
+# Logo Tab
+logo_button = customtkinter.CTkButton(master=tabview.tab("Logo Watermark"), text="Open image", command=image_loc,
+                                      width=200)
+logo_button.place(y=15, x=95)
+
+logo_size = customtkinter.CTkComboBox(master=tabview.tab("Logo Watermark"), values=["1", "2"], width=60)
+logo_size.place(y=55, x=235)
+
+logo_position = customtkinter.CTkComboBox(master=tabview.tab("Logo Watermark"),
+                                          values=["adsadasdasdasdasdasd", "b", "c"], width=130)
+logo_position.place(y=55, x=95)
 
 window.mainloop()

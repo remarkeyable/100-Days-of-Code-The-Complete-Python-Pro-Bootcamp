2e1f6107f7becece0c0ba07e791908e0bfdf902e
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-05-30 21:53:01 +0800

    Update main.py

--- Day 85/main.py
+++ Day 85/main.py
@@ -17,42 +17,26 @@ window.title("Typing Speed Test")
 
 chosen_word = ""
 def start(event):
-        # print(chosen_word)
-        # print(len(chosen_word) -1)
-        # print(len(entry.get() + event.char))
-        if len(chosen_word) -1  < len(entry.get() + event.char):
-            print(entry.get() + event.char)
-            if chosen_word != str(entry.get() + event.char):
-                words.configure(fg_color="#CD1818")
-            else:
-            # elif chosen_word == str(entry.get() + event.char):
-                print("yesy")
-                generate_word()
-                words.configure(text= chosen_word)
-        elif chosen_word == str(entry.get()):
-            print("yesy")
-        # else:
-        #     words.configure(fg_color="transparent")
+        global chosen_word, entry
+        if chosen_word == entry.get() + event.char:
+            print("hello")
+            generate_word()
+            entry.delete(0, END)
+            words.configure(text=chosen_word)
+
+
+
+       
 
 
 def generate_word():
     global chosen_word
     with open('words.csv') as text:
-        choice = random.choice(list(text))
-        chosen_word = choice
+        words = list(text)
+        chosen_word = random.choice(words).strip()
+
 
 generate_word()
-    # if text != entry.get():
-    #     print(entry.get() + event.char)
-    #     label.configure(text= event.char, text_color="#B70404")
-    # else:
-    #     label.configure(text=entry.get(), text_color="#27374D")
-
-    # label.configure(text= entry.get())
-
-
-text = "hello"
-
 words = customtkinter.CTkLabel(window,text=chosen_word)
 words.pack()
 
@@ -61,6 +45,7 @@ label.pack()
 
 entry = customtkinter.CTkEntry(window)
 entry.pack()
+
 
 start_t = customtkinter.CTkButton(window, text="start",command=start)
 start_t.pack()

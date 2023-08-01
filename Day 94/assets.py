51a66b89a56a2c55ec7044b0345e78e3781c5374
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-08-01 16:32:24 +0800

    Add files via upload

--- Day 94/assets.py
+++ Day 94/assets.py
@@ -1,24 +1,55 @@
 import pygame
 import os
 
+pygame.font.init()
+
 BG_COLOR = (14, 41, 84)
+
+# Aliens
+ALIEN1 = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'alien1.png')), 270)
+ALIEN2 = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'alien2.png')), 270)
+ALIEN3 = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'alien3.png')), 270)
+
+# Ship
 SHIP = pygame.image.load(os.path.join('Assets', 'spaceship.png'))
 BULLET = pygame.transform.rotate(pygame.image.load(os.path.join('Assets', 'bullet.png')), 90)
 SPACE_SHIP = pygame.transform.scale(SHIP, (60, 60))
 THE_BUL = []
+
+# Background
+BG = pygame.image.load(os.path.join('Assets', 'space_bg.png'))
+
+
+class Laser:
+    def __init__(self, x, y, image):
+        self.x = x
+        self.y = y
+        self.lasers = image
+        self.mask = pygame.mask.from_surface(self.lasers)
+
+    def move(self, vel):
+        self.y += vel
+
+    def draw(self, window, x, y):
+        window.blit(self.lasers, (x, self.y))
 
 
 class Assets:
     def __init__(self):
         self.window = pygame.display.set_mode((600, 600))
         pygame.display.set_caption('Space Invader')
+        self.fps = 60
         self.ship = pygame.Rect(295, 500, 60, 60)
         self.bullet = pygame.Rect(self.ship.x, self.ship.y, 60, 60)
-
+        self.bul = None
+        self.action = None
         self.fired = []
         self.run = True
         self.num = 0
         self.clock = pygame.time.Clock()
+        self.font = pygame.font.SysFont('comicsans', 15)
+        self.score = 0
+        self.lives = 5
 
     def ship_movement(self):
         keys_pressed = pygame.key.get_pressed()
@@ -34,16 +65,30 @@ class Assets:
         elif keys_pressed[pygame.K_DOWN] and self.ship.y < 550:
             self.ship.y += 3
             self.bullet.y += 3
-        elif keys_pressed[pygame.K_SPACE]:
-            bul = self.window.blit(BULLET, (self.bullet.x, self.bullet.y))
-            self.fired.append(bul)  # self.fire_bullet()
+        elif keys_pressed[pygame.K_a]:
+            en = Laser(self.bullet.x, self.bullet.y, BULLET)
+            # en.draw(self.window,self.bullet.x,self.bullet.y)
+            self.fired.append(en)
 
     def update_window(self):
-        self.window.fill(BG_COLOR)
-        self.window.blit(SPACE_SHIP, (self.ship.x, self.ship.y))
+
+        self.window.blit(BG, (0, 0))
         self.fire_bullet()
+        self.window.blit(SPACE_SHIP, (self.ship.x, self.ship.y))
+        self.action = None
+
+        lives_text = self.font.render(f"Lives: {self.lives}", True, (255, 255, 255))
+        score_text = self.font.render(f"Score: {self.score}", True, (255, 255, 255))
+        self.window.blit(lives_text, (10, 10))
+        self.window.blit(score_text, (600 - score_text.get_width() - 10, 10))
+
         pygame.display.update()
 
     def fire_bullet(self):
         for i in self.fired:
-            self.bullet.y -= 1
+            i.draw(self.window, self.bullet.x, self.bullet.y)
+            i.y -= 10
+
+    def move_lasers(self, vel):
+        for i in self.fired:
+            i.move(vel)
--- Day 94/main.py
+++ Day 94/main.py
@@ -3,21 +3,23 @@ import os
 import time
 from assets import Assets
 
-FPS = 60
 assets = Assets()
 
 
 def main():
     while assets.run:
-        assets.clock.tick(FPS)
+
+        assets.clock.tick(assets.fps)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 assets.run = False
         assets.ship_movement()
         assets.update_window()
         keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_SPACE]:
-            assets.fire_bullet()
+        # if keys_pressed[pygame.K_SPACE]:
+        #     assets.fire_bullet()
+
+
     pygame.quit()
 
 

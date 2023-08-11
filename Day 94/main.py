64e8411061e6ddb8bc3995811f55a1dd48bad09a
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-08-11 16:06:12 +0800

    Add files via upload

--- Day 94/main.py
+++ Day 94/main.py
@@ -1,6 +1,6 @@
 import pygame
 from assets import Assets
-
+pygame.init()
 assets = Assets()
 
 
@@ -12,8 +12,33 @@ def main():
         assets.update_window()
         if assets.lives <= 0:
             assets.restart_game()
+            you_lost()
+    pygame.quit()
+
+def start():
+    run = True
+    while run:
+        assets.game_start()
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
+    pygame.quit()
+
+def you_lost():
+    run = True
+    assets.over_sound()
+    while run:
+        assets.game_over()
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                main()
     pygame.quit()
 
 
-if __name__ == "__main__":
-    main()
+start()

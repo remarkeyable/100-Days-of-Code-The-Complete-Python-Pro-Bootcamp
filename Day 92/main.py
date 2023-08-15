af64ba1a67ea5ac98cd2f12e2744ad886d965b3d
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-08-15 09:56:15 +0800

    Update main.py

--- Day 92/main.py
+++ Day 92/main.py
@@ -43,6 +43,7 @@ num = 0
 the_limit = None
 number_of_pages = None
 
+
 # def limit():
 #     global the_limit
 #     start = driver.find_element(By.XPATH, '//*[@id="top-1"]/div/div/div/header/div[1]/span/nav/span/ul/li/a')
@@ -55,64 +56,67 @@ number_of_pages = None
 
 #
 def pressed():
-    global num, the_limit, count, number_of_pages
-    print(the_limit)
+    global num, the_limit, count, number_of_pages, books
 
-    start = driver.find_element(By.XPATH, '//*[@id="top-1"]/div/div/div/header/div[1]/span/nav/span/ul/li/a')
-    start.send_keys(Keys.TAB)
-    start.send_keys(Keys.END)
-    the_tittle = driver.find_elements(By.ID, 'center-3')
-    for i in the_tittle:
-        bc_item = i.find_elements(By.CLASS_NAME, 'bc-list-item')
-        bc_link = i.find_elements(By.CLASS_NAME, 'bc-link')
-        bc_price = i.find_elements(By.CLASS_NAME, 'bc-text')
-        for j in bc_item:
-            title = j.get_attribute('aria-label')
-            if title == None:
-                pass
-            else:
-                book_title.append(title)
+    try:
+        start = driver.find_element(By.XPATH, '//*[@id="top-1"]/div/div/div/header/div[1]/span/nav/span/ul/li/a')
+        start.send_keys(Keys.TAB)
+        start.send_keys(Keys.END)
+        the_tittle = driver.find_elements(By.ID, 'center-3')
+        for i in the_tittle:
+            bc_item = i.find_elements(By.CLASS_NAME, 'bc-list-item')
+            bc_link = i.find_elements(By.CLASS_NAME, 'bc-link')
+            bc_price = i.find_elements(By.CLASS_NAME, 'bc-text')
+            for j in bc_item:
+                title = j.get_attribute('aria-label')
+                if title == None:
+                    pass
+                else:
+                    book_title.append(title)
 
-        for k in bc_link:
-            link = k.get_attribute('href')
-            book_link.append(link)
+            for k in bc_link:
+                link = k.get_attribute('href')
+                book_link.append(link)
 
-        for l in bc_price:
-            price = l.text
-            if "$" in price and "Regular" not in price:
-                book_price.append(price)
+            for l in bc_price:
+                price = l.text
+                if "$" in price and "Regular" not in price:
+                    book_price.append(price)
 
-        for m in range(len(book_title)):
-            bk = {'Book': book_title[m], 'Price': book_price[m], 'Link': book_link[m]}
-            books.append(bk)
+            for m in range(len(book_title)):
+                bk = {'Book': book_title[m], 'Price': book_price[m], 'Link': book_link[m]}
+                books.append(bk)
 
-    if the_limit == None:
-        lim = driver.find_element(By.XPATH, '//*[@id="pagination-a11y-skiplink-target"]/div/div[2]/div/span/ul/li[5]/a')
-        the_limit = int(lim.text)-3
-        number_of_pages = int(lim.text)
+        if the_limit == None:
+            lim = driver.find_element(By.XPATH,
+                                      '//*[@id="pagination-a11y-skiplink-target"]/div/div[2]/div/span/ul/li[5]/a')
+            the_limit = int(lim.text) - 3
+            number_of_pages = int(lim.text)
+
+        page = driver.find_element(By.XPATH,
+                                   f'//*[@id="pagination-a11y-skiplink-target"]/div/div[2]/div/span/ul/li[{the_limit}]/a')
+        page.click()
+        num += 1
+        count += 1
+        if the_limit != number_of_pages:
+            the_limit += 1
+    except:
+        num += 1
 
 
-    page = driver.find_element(By.XPATH,f'//*[@id="pagination-a11y-skiplink-target"]/div/div[2]/div/span/ul/li[{the_limit}]/a')
-    page.click()
-    num += 1
-    count +=1
-    if the_limit != number_of_pages:
-        the_limit += 1
-
+def export_csv():
+    with open(the_books, mode="w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(['Book', 'Price', 'Link'])
+        for book in books:
+            writer.writerow([book['Book'], book['Price'], book['Link']])
 
 
 on = True
 
 while on:
     pressed()
-    if num == the_limit -1:
+    print(num, the_limit)
+    if num == the_limit:
         on = False
-        print(books)
-        print(count)
-        with open(the_books, mode="w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(['Book', 'Price', 'Link'])
-            for book in books:
-                writer.writerow([book['Book'], book['Price'], book['Link']])
-
-
+        export_csv()

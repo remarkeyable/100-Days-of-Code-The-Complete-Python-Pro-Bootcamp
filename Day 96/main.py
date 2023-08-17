b1ae9c5fdfe1d6981073050944d0429d19309d7d
Ryn <122131469+remarkeyable@users.noreply.github.com>
2023-08-17 16:45:20 +0800

    Update main.py

--- Day 96/main.py
+++ Day 96/main.py
@@ -1,6 +1,6 @@
 import requests
 import wget
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, send_from_directory
 from flask_bootstrap import Bootstrap5
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
@@ -15,7 +15,7 @@ Bootstrap5(app)
 # Forms
 class Search(FlaskForm):
     search = StringField('Tiktok Link', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+    submit = SubmitField('Download')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,28 +24,26 @@ def home():
 
     if form.validate_on_submit():
         tiktok_link = form.search.data
-        file_name = wget.filename_from_url(tiktok_link)
-        wget.download(tiktok_link, file_name)
+
+        # API
+        url = "https://tiktok-video-no-watermark2.p.rapidapi.com/"
+
+        querystring = {"url": f"{tiktok_link}", "hd": "1"}
+
+        headers = {"X-RapidAPI-Key": "a3fc281e43msh2eaa11b1b32676ep139a38jsn52e35c774548",
+                   "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com"}
+
+        response = requests.get(url, headers=headers, params=querystring)
+
+        no_watermark = response.json()['data']['play']
+        print(no_watermark)
 
         download_folder = os.path.expanduser('~') + '\Downloads'
-        os.rename(file_name, download_folder)
-        return redirect(url_for('home'))
+        filename = "test.mp4"
+        wget.download(no_watermark, os.path.join(download_folder, filename))
+        return redirect(url_for('home'))  # return send_from_directory(download_folder, filename, as_attachment=True)
 
     return render_template('index.html', form=form)
-
-
-# url = "https://tiktok-video-no-watermark2.p.rapidapi.com/"
-#
-# querystring = {"url": "https://www.tiktok.com/@marianrivera/video/7265653123367161094?lang=en", "hd": "1"}
-#
-# headers = {"X-RapidAPI-Key": "a3fc281e43msh2eaa11b1b32676ep139a38jsn52e35c774548",
-#            "X-RapidAPI-Host": "tiktok-video-no-watermark2.p.rapidapi.com"}
-#
-# response = requests.get(url, headers=headers, params=querystring)
-#
-# no_watermark = response.json()['data']['play']
-#
-# print(no_watermark)
 
 
 if __name__ == '__main__':
